915889d474e079fa155f35a3efc6367f72fc037b
annu18coder <aditi8ananya@gmail.com>
2026-06-15 09:30:20 +0530

    fixed folder structure

--- Backend/Frontend/src/Components/1Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar(){
-    return(
-        <div>
-            <h1>Logo</h1>    
-            <button>Enroll Now</button>
-        </div>
-    );
-};
-export default Navbar;
--- Backend/Frontend/src/Components/7Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <div>
-            <h1>Logo</h1>    
-            <p>Call To Action</p>
-        </div>
-    );
-};
-export default Footer;
